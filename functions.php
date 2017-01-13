b842126f04e64b7d8238c0b287b6f6985fdf0137
Matt McCullough <mmccullough@blackjet.ca>
2017-01-13 17:17:30 -0500

    Small to renaming files and functions

--- functions.php
+++ functions.php
@@ -20,19 +20,32 @@ function bjzm_theme_setup() {
 }
 add_action( 'after_setup_theme', 'bjzm_theme_setup' );
 
-
+/**
+ * Scripts & Styles
+ */
 if ( file_exists( dirname( __FILE__ ) . '/includes/bjzm-scripts-styles.php' ) ) {
 	require_once( dirname(__FILE__).'/includes/bjzm-scripts-styles.php' );
 }
 
-
+/**
+ * Theme Options
+ */
 if ( file_exists( dirname( __FILE__ ) . '/includes/bjzm-options.php' ) ) {
 	require_once( dirname(__FILE__).'/includes/bjzm-options.php' );
 }
 
-
+/**
+ * Instagram and Twitter API Feeds
+ */
 if ( file_exists( dirname( __FILE__ ) . '/includes/bjzm-social-feeds.php' ) ) {
 	require_once( dirname(__FILE__).'/includes/bjzm-social-feeds.php' );
+}
+
+/**
+ * Theme Functions
+ */
+if ( file_exists( dirname( __FILE__ ) . '/includes/bjzm-theme-functions.php' ) ) {
+	require_once( dirname(__FILE__).'/includes/bjzm-theme-functions.php' );
 }
 
 
