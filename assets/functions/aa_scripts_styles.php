7cb1c834bcbaa022cd7ab4c33dab4605c43096e4
zsawaf <zsawaf@blackjet.ca>
2017-04-10 13:53:16 -0400

    New commit

--- assets/functions/aa_scripts_styles.php
+++ assets/functions/aa_scripts_styles.php
@@ -120,6 +120,12 @@ function aa_styles()
 
 }
 
+// Update CSS within in Admin
+function admin_style() {
+  wp_enqueue_style('admin-styles', get_template_directory_uri().'/assets/css/admin.css');
+}
+add_action('admin_enqueue_scripts', 'admin_style');
+
 /**
  *
  * Comment Reply js to load only when thread_comments is active
---
+++ views/content-contact.php
@@ -0,0 +1,5 @@
+<div class="contact">
+	<div class="contact__form">
+		
+	</div>
+</div>
