50ad90c7e208600d67198ae9adb878715501c651
Matt McCullough <mmccullough@blackjet.ca>
2017-04-19 16:05:14 -0400

    post nav, text styles

--- wp-content/themes/bjzm-starter-theme/views/content-card.php
+++ wp-content/themes/bjzm-starter-theme/views/content-card.php
@@ -2,33 +2,33 @@
 	
 	<div class="blog-post-inner l-card-inner">
 
-			<div class="card__image">
-				<?php if( has_post_thumbnail() ) : ?>
-				<a href="<?php the_permalink(); ?>">
-					<picture>
-						<source srcset="<?php echo get_the_post_thumbnail_url( null, 'single-featured-image-mobile') ?>"  media="(max-width: 480px)">
-						<img class="slider-img" src="<?php echo get_the_post_thumbnail_url( null, 'card-featured-image') ?>" alt="">
-					</picture>
-				</a>
-				<?php endif; ?>
+		<div class="card__image">
+			<?php if( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>">
+				<picture>
+					<source srcset="<?php echo get_the_post_thumbnail_url( null, 'single-featured-image-mobile') ?>"  media="(max-width: 480px)">
+					<img class="slider-img" src="<?php echo get_the_post_thumbnail_url( null, 'card-featured-image') ?>" alt="">
+				</picture>
+			</a>
+			<?php endif; ?>
+		</div>
+
+		<header class="card__header post-header post-header--loop">
+			<p class="post-header__category"><?php echo bjzm_get_post_category() ?></p>
+			<h2 class="post-header__title"><?php echo get_the_title(); ?></h2>
+		</header>
+
+		<div class="card__content">
+
+			<div class="card__excerpt">
+				<p><?php echo get_the_excerpt(); ?></p>
 			</div>
 
-			<header class="card__header post-header post-header--loop">
-				<p class="post-header__category"><?php echo bjzm_get_post_category() ?></p>
-				<h2 class="post-header__title"><?php echo get_the_title(); ?></h2>
-			</header>
-
-			<div class="card__content">
-	
-				<div class="card__excerpt">
-					<p><?php echo get_the_excerpt(); ?></p>
-				</div>
-
-				<div class="card__read-more">
-					<a href="<?php the_permalink(); ?>" class="button">Read More</a>
-				</div>
-
+			<div class="card__read-more">
+				<a href="<?php the_permalink(); ?>" class="button">Read More</a>
 			</div>
+
+		</div>
 
 	</div>
 
--- wp-content/themes/bjzm-starter-theme/views/post-navigation.php
+++ wp-content/themes/bjzm-starter-theme/views/post-navigation.php
@@ -10,7 +10,7 @@ $next_post = get_adjacent_post( null, '', false);
 		<div class="post-nav__adjacent post-nav__adjacent--left">
 		<?php if ($prev_post): ?>
 			<a href="<?php echo get_permalink( $prev_post->ID ) ?>" title="<?php echo $prev_post->post_title; ?>" class="post-nav__link post-nav__link--left">
-				<span>Back</span>
+				<span>Previous Post</span>
 			</a>
 		<?php endif; ?>
 		</div>
@@ -18,7 +18,7 @@ $next_post = get_adjacent_post( null, '', false);
 		<div class="post-nav__adjacent post-nav__adjacent--right">
 		<?php if ($next_post): ?>
 			<a href="<?php echo get_permalink( $next_post->ID ) ?>" title="<?php echo $next_post->post_title; ?>" class="post-nav__link post-nav__link--right">
-				<span>Next</span> 
+				<span>Next Post</span> 
 			</a>
 		<?php endif ?>
 		</div>
