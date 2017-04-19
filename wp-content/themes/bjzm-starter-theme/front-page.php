b87500b41f4a1f9e7de061295cec7d05f5d4bfc0
Matt McCullough <mmccullough@blackjet.ca>
2017-04-19 11:39:42 -0400

    Various updates to loop on homepage

--- wp-content/themes/bjzm-starter-theme/front-page.php
+++ wp-content/themes/bjzm-starter-theme/front-page.php
@@ -4,13 +4,13 @@
 	
 	<div class="blog-post-wrapper container">
 		<div class="row">
+
 			<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'views/content-card' ); ?>
 			
-			<?php endwhile; ?>
-
-			<?php endif; ?>
+			<?php endwhile; ?><?php endif; ?>
+			
 		</div>
 	</div>
 
