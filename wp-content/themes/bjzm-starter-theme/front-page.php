b6d4ab3506ac96554d43d453b76010dbf9525801
Matt McCullough <mmccullough@blackjet.ca>
2017-04-21 18:11:59 -0400

    Starting a new template for Hero Grid layout

--- wp-content/themes/bjzm-starter-theme/front-page.php
+++ wp-content/themes/bjzm-starter-theme/front-page.php
@@ -9,14 +9,12 @@
 	<div class="l-bjzm-main-wrapper-inner">
 
 		<div class="home-wrapper container">
-			
 
 			<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'views/content-home' ); ?>
 			
 			<?php endwhile; ?><?php endif; ?>
-			
 
 		</div>
 		
