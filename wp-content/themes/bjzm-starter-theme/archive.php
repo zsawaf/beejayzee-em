87640f13b99279bbdcc235ea0b9a154a6b9a095b
Matt McCullough <mmccullough@blackjet.ca>
2017-04-24 12:51:17 -0400

    Make markup consistent between theme files

--- wp-content/themes/bjzm-starter-theme/archive.php
+++ wp-content/themes/bjzm-starter-theme/archive.php
@@ -1,24 +1,35 @@
 <?php get_header(); ?>
 
-<div class="bjzm-wrapper bjzm-wrapper--archive">
+<div class="bjzm-wrapper bjzm-wrapper--index">
 	
-	<?php get_template_part( 'views/slideshow' ) ?>
-
 	<div class="bjzm-wrapper__inner">
 
-		<div class="blog-post-wrapper container">
-			<div class="row">
-
-				<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'views/content-card' ); ?>
-				
-				<?php endwhile; ?><?php endif; ?>
-				
-			</div>
-		</div>
+	<?php do_slideshow(16, "slider", "home_slider"); ?>
 		
-	</div>
-</div>
+		<div class="bjzm-content">
+			
+			<div class="bjzm-content__container container">
+				
+				<div class="bjzm-content__row row">
+					
+					<div class="post-loop post-loop--archive">
+
+					<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'views/content-card' ); ?>
+					
+					<?php endwhile; ?><?php endif; ?>
+				
+					</div><!-- .post-loop -->
+
+				</div><!-- .bjzm-content__row -->
+
+			</div><!-- .bjzm-content__container -->
+
+		</div><!-- .bjzm-content -->
+		
+	</div><!-- .bjzm-wrapper__inner -->
+
+</div><!-- .bjzm-wrapper -->
 
 <?php get_footer(); ?>
--- wp-content/themes/bjzm-starter-theme/index.php
+++ wp-content/themes/bjzm-starter-theme/index.php
@@ -6,31 +6,31 @@
 	<div class="bjzm-wrapper__inner">
 
 		<?php do_slideshow(16, "slider", "home_slider"); ?>
-
+		
 		<div class="bjzm-content">
-
-			<div class="bjzm-content__container container">
 			
+			<div class="bjzm-content__container container">
+				
 				<div class="bjzm-content__row row">
-
+					
 					<div class="post-loop post-loop--home">
 
-						<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
+					<?php if ( have_posts() ) :  while ( have_posts() ) : the_post(); ?>
 
-							<?php get_template_part( 'views/content-card' ); ?>
-						
-						<?php endwhile; ?><?php endif; ?>
+						<?php get_template_part( 'views/content-card' ); ?>
+					
+					<?php endwhile; ?><?php endif; ?>
 
-					</div>
+					</div><!-- .post-loop -->
 
-				</div>
+				</div><!-- .bjzm-content__row  -->
 
-			</div>
+			</div><!-- .bjzm-content__container -->
 
-		</div>
+		</div><!-- .bjzm-content -->
 		
-	</div>
+	</div><!-- .bjzm-wrapper__inner -->
 
-</div>
+</div><!-- .bjzm-wrapper -->
 
 <?php get_footer(); ?>
